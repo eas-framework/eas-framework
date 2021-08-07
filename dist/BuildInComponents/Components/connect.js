8293338cf9fbf1bad1c066f8a4724389309d4a16
Ido s <ido_do@outlook.com>
2021-08-07 18:45:32 +0300

    New Component - form (With connector to the server)

--- dist/BuildInComponents/Components/connect.js
+++ dist/BuildInComponents/Components/connect.js
@@ -50,7 +50,7 @@ export function addFinalizeBuild(pageData, sessionInfo) {
             }
         }`;
     if (pageData.includes("@ConnectHere"))
-        pageData = pageData.replacer(/@ConnectHere(;?)/, () => new StringTracker('@ConnectHere;', addScript));
+        pageData = pageData.replacer(/@ConnectHere(;?)/, () => new StringTracker(StringTracker.emptyInfo, addScript));
     else
         pageData.AddTextAfter(addScript);
     return pageData;
