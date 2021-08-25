2f0103a9217ca329acae214989ab5b3d80f6f26b
Ido s <ido_do@outlook.com>
2021-08-25 11:25:44 +0300

    Fix bug - 8/25/2021, 11:25:44 AM

---
+++ build/writeGitLabCi.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import {promises} from 'fs';
+
+const file = path.resolve() + '/.gitlab-ci.yml';
+let content = await promises.readFile(file, 'utf8');
+
+content = content.replace(/"[\W\w]?"/, `"${process.argv[2]} - auto build"`);
+promises.writeFile(file, content);
+
