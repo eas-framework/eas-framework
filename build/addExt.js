92a2b0de1a8a93b513d3154bc2ea0e3592eb9424
Ido s <ido_do@outlook.com>
2021-11-11 13:25:52 +0200

    fix addExt tool

--- build/addExt.js
+++ build/addExt.js
@@ -30,19 +30,19 @@ async function searchDir(path){
 async function fixImports(filepath){
     let content = await promises.readFile(filepath, 'utf8');
 
-    const mathRegex = /import[ ]+[\*]{0,1}[\p{L}0-9_,\{\} ]+[ ]+from[ ]+(['|`|"])([A-Za-z0-9_\-@\$\./\\]+)\1/gu;
+    const mathRegex = /(import|export)[ ]+[\*]{0,1}[\p{L}0-9_,\{\} ]+[ ]+(from[ ]+)?(['|`|"])([A-Za-z0-9_\-@\$\./\\]+)\3/gu;
 
     content = content.replace(mathRegex, (...match) => {
-        const filePath = match[2];
+        const filePath = match[4];
 
-        if((filePath[0] == '.' || filePath[0] == '/') && !path.extname(filePath)){
-            let newImport = match[0].substring(0, match[0].indexOf(match[1])); // the start of the import
+        if ((filePath[0] == '.' || filePath[0] == '/') && !path.extname(filePath)) {
+            let newImport = match[0].substring(0, match[0].indexOf(match[3])); // the start of the import
 
-            newImport += match[1] + filePath + '.js' + match[1];
+            newImport += match[3] + filePath + '.js' + match[3];
 
             return newImport;
         }
-        
+
         return match[0];
     });
 
