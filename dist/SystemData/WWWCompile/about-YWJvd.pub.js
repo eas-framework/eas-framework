a8254f6e7c25edf5de969b6bdbb75c5314f7eac9
Ido S <ido.pluto@gmail.com>
2022-02-15 22:34:47 +0200

    small-path errors

--- dist/SystemData/WWWCompile/about-YWJvd.pub.js
+++
@@ -1,4 +0,0 @@
-
-         var b = 9;
-        
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL01vZGVscy9XZWJzaXRlLm1vZGUuc291cmNlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFZQTtBQUNBIiwiZmlsZSI6ImFib3V0LnBhZ2UifQ==
