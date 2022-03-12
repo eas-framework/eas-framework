38c63f382c51afb377de7a1ff00904cee2e955fb
Ido S <ido.pluto@gmail.com>
2022-03-12 13:58:43 +0200

    try postinstall

--- src/scripts/install.ts
+++ src/scripts/install.ts
@@ -1,6 +1,7 @@
-import { chdir } from "process";
+import { chdir, cwd } from "process";
 import { autoCodeTheme, minifyMarkdownTheme } from "../BuildInComponents/Components/markdown";
 
+console.log(cwd());
 chdir('../')
 await minifyMarkdownTheme();
 await autoCodeTheme();
