788a15332bb2c39f61fca15ee574abb29233101f
Ido S <ido.pluto@gmail.com>
2022-03-12 14:12:49 +0200

    postinstall fix

--- dist/scripts/install.js
+++ dist/scripts/install.js
@@ -1,6 +1,7 @@
-import { chdir } from "process";
+import { chdir, cwd } from "process";
 import { autoCodeTheme, minifyMarkdownTheme } from "../BuildInComponents/Components/markdown.js";
-chdir('../');
+if (cwd().split('/').at(-2) == 'node_modules')
+    chdir('../../');
 await minifyMarkdownTheme();
 await autoCodeTheme();
 //# sourceMappingURL=install.js.map
