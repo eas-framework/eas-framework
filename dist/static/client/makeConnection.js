e49e1ed4b4bcdf75022cd9fc00a99d9f0de9ddf4
Ido S <ido.pluto@gmail.com>
2022-03-20 15:04:37 +0200

    undefined

--- dist/static/client/makeConnection.js
+++ dist/static/client/makeConnection.js
@@ -1,1 +1,16 @@
-async function connector(n,t){return await(await fetch(location.href,{method:"POST",body:JSON.stringify({connectorCall:{name:n,values:t}}),headers:{"Content-Type":"application/json"}})).json()}
+async function connector(name, values) {
+    const data = await fetch(location.href, {
+        method: 'POST',
+        body: JSON.stringify({
+            connectorCall: {
+                name,
+                values
+            }
+        }),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    });
+
+    return await data.json();
+}
