1ecb1f7eb19dc1245aed27f60516e60715b3a8f3
Ido S <ido.pluto@gmail.com>
2022-03-20 14:52:17 +0200

    better ci

--- build/index.js
+++ build/index.js
@@ -1,15 +1,13 @@
-import {exec} from 'child_process';
 import path from 'path';
 import fsExtra from 'fs-extra';
-const {copy, remove, mkdir} = fsExtra;
+const {emptyDir} = fsExtra;
 
 const __dirname = path.resolve(), dist =  __dirname + '/dist/';
 
 console.log('building...');
 
 // deleting the content in dist directory
-await remove(dist);
-await mkdir(dist);
+await emptyDir(dist);
 
 await import('./build.js');
 
