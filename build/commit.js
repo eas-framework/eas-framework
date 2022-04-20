5f0cc4f0db42dd0b0dd1c34c6dd595db2445f34b
Ido S <ido.pluto@gmail.com>
2022-04-20 12:30:05 +0300

    build: remove unused build tools

--- build/commit.js
+++
@@ -1,5 +0,0 @@
-import {StreamCommand, args} from './tools/StreamCommand.js';
-
-await StreamCommand('git add .');
-await StreamCommand(`git commit -am "${args.first ?? "Fix bug - " + new Date().toLocaleString()}"`);
-await StreamCommand('git push');
--- build/production.js
+++
@@ -1,4 +0,0 @@
-import {StreamCommand, args} from './tools/StreamCommand.js';
-
-await StreamCommand('npm run build production');
-await StreamCommand(`npm run commit "${args.first}"`);
